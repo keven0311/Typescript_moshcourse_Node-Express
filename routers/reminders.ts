7e4a6bb1449fd973bc61393998191aefd09d9d89
kaichong lin <coco.keven@gmail.com>
2023-08-24 04:29:27 -0500

    delete route and put route added

--- routers/reminders.ts
+++ routers/reminders.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import CreateReminderDto from '../dtos/create-reminder'
 import DeleteReminderDto from '../dtos/delete-reminder'
 import Reminder from '../models/reminder'
+import PutReminderDto from '../dtos/put-reminder'
 
 
 const router = Router()
@@ -23,6 +24,17 @@ router.delete('/:id', (req,res) => {
     const  id  = req.params.id
     const newReminders = reminders.filter(reminder => reminder.id !== parseInt(id))
     res.json(newReminders)
+})
+
+router.put('/:id', (req,res) => {
+    const { title } = req.body as PutReminderDto
+    const reminderIdx = reminders.findIndex(reminder => reminder.id === parseInt(req.params.id) )
+    if(reminderIdx >= 0){
+        reminders[reminderIdx].title = title
+        res.json(reminders[reminderIdx])
+    }else{
+        res.status(404).json({message:'Reminder not found'})
+    }
 })
 
 export default router
